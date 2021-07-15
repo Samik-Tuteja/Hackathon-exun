39c674c14d683f91e15f166864abf538b56773f5
Samik-coder <e10371samik@dpsrkp.net>
2021-07-15 21:54:15 +0530

    Innitial Commit

---
+++ js/signup.js
@@ -0,0 +1,39 @@
+const header = document.querySelector(".navbar");
+const username = document.querySelector(".username");
+const password = document.querySelector(".pass");
+const usernameError = document.querySelector(".username-error");
+const passwordError = document.querySelector(".password-error");
+const resetPass = document.querySelector(".reset-pass");
+const resetPassError = document.querySelector(".reset-password-error");
+
+window.addEventListener("scroll", () => {
+  const scrollPos = window.scrollY;
+  if (scrollPos > 100) {
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
+  }
+});
+
+document
+  .querySelector(".submit-btn")
+  .addEventListener("click", function (event) {
+    passwordError.textContent = "";
+    usernameError.textContent = "";
+    resetPassError.textContent = "";
+    if (username.value.trim() === "") {
+      usernameError.textContent = "Please fill the username";
+      event.preventDefault();
+    } else if (password.value.trim() === "") {
+      passwordError.textContent = "Please fill the password";
+      event.preventDefault();
+    } else if (resetPass.value.trim() === "") {
+      resetPassError.textContent = "Please fill the password";
+      event.preventDefault();
+    } else if (resetPass.value !== password.value) {
+      resetPassError.textContent = "Passwords do not maatch";
+      event.preventDefault();
+    } else {
+      location = bank.html;
+    }
+  });
